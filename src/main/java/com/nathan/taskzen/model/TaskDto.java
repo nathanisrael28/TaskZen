bcac722aaa515532bba0099fe5d92f98e51c70ce
nathanisrael28 <nathanisrael11@gmail.com>
2025-06-26 11:21:02 +0530

    Sprint 2 Completed

--- src/main/java/com/nathan/taskzen/model/TaskDto.java
+++ src/main/java/com/nathan/taskzen/model/TaskDto.java
@@ -1,28 +1,37 @@
 package com.nathan.taskzen.model;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import com.nathan.taskzen.enums.Status;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDate;
 
 // We can call DTO( Data transfer Object) as Bean/BussinessBean and Model also
+@Schema(description = "Data transfer object for Task")
 public class TaskDto {
 
 
+    @Schema(description = "Unique ID of the task", example = "101")
     private Long id;
 
     @NotBlank(message = "Title is required")
     @Size(min = 3, message = "Title must be at least 3 characters")
+    @Schema(description = "Title of the task", example = "Complete Swagger docs")
     private String title;
-
+    @Schema(description = "Description of the task", example = "Add API annotations for better documentation")
     private String description;
 
     @FutureOrPresent(message = "Due Date Can't be in Past Date")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @Schema(description = "Due date in dd-MM-yyyy format", example = "01-07-2025")
     private LocalDate dueDate;
 
+    @Schema(description = "Status of the task", example = "PENDING")
     @Enumerated(EnumType.STRING)
     private Status status;
 
@@ -88,6 +97,9 @@ public class TaskDto {
                 ", status=" + status +
                 '}';
     }
+
+
+
 }
 
 
